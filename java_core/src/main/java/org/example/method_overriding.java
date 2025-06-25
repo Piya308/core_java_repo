82eafc2683f6171d99cfdee7efaa788ff48afcdc
Piya308 <priyankagaikwad1185@gmail.com>
2025-06-25 23:11:57 +0530

    overriding example

---
+++ java_core/src/main/java/org/example/method_overriding.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class method_overriding {
+    public static void main(String[] args) {
+
+    }
+
+class Animal{
+        void food(){
+
+        }
+}
+
+class B extends Animal{
+        void food (int a){
+        }
+}
+}
